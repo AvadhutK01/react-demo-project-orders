8fb4242abde3b66931dc2371ea9aa6071471d0b2
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-09 18:48:40 +0530

    Some minor changes

--- src/components/input component/InputComponent.jsx
+++ src/components/input component/InputComponent.jsx
@@ -9,23 +9,27 @@ const InputComponent = (props) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         props.addOrder(Id, Amount, Dish, Table);
+        setId("");
+        setAmount("");
+        setDish("");
+        setTable("");
     }
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <input type='text' placeholder='Enter Unique order Id' value={Id} onChange={(e) => setId(e.target.value)} />
-                <input type='number' placeholder='Enter Amount' value={Amount} onChange={(e) => setAmount(e.target.value)} />
-                <select value={Dish} onChange={(e) => setDish(e.target.value)}>
+                <input type='text' required placeholder='Enter Unique order Id' value={Id} onChange={(e) => setId(e.target.value)} />
+                <input type='number' required placeholder='Enter Amount' value={Amount} onChange={(e) => setAmount(e.target.value)} />
+                <select value={Dish} required onChange={(e) => setDish(e.target.value)}>
                     <option value="">Choose dish</option>
                     <option value="Chicken Biryani">Biryani</option>
                     <option value="Mutton Biryani">Mutton Biryani</option>
                     <option value="Chicken Tikka Masala">Chicken Tikka Masala</option>
                 </select>
-                <select value={Table} onChange={(e) => setTable(e.target.value)}>
+                <select value={Table} required onChange={(e) => setTable(e.target.value)}>
                     <option value="">Choose table</option>
-                    <option value="Table 1">Table 1</option>
-                    <option value="Table 2">Table 2</option>
-                    <option value="Table 3">Table 3</option>
+                    <option value="Table1">Table 1</option>
+                    <option value="Table2">Table 2</option>
+                    <option value="Table3">Table 3</option>
                 </select>
                 <button type='submit'>Add</button>
             </div>
