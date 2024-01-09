dc1e727ed0500273c6f59f98d27267ddcb3a6e15
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-09 18:39:31 +0530

    Implemented list component

--- src/components/list component/ListComponent.jsx
+++ src/components/list component/ListComponent.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-const ListComponent = () => {
+const ListComponent = (props) => {
+    const { Orders } = props;
+
+    const renderTableOrders = (table) => {
+        const tableOrders = Orders[table];
+
+        return (
+            <ul>
+                {Object.entries(tableOrders).map(([orderId, order]) => (
+                    <li key={orderId}>
+                        {`Order ID: ${orderId}, Amount: ${order.Amount}, Dish: ${order.Dish}`}
+                        <button onClick={() => props.deleteOrder(orderId, table)}>Delete Order</button>
+                    </li>
+                ))}
+            </ul>
+        );
+    };
+
     return (
-        <div>ListComponent</div>
-    )
-}
+        <>
+            <div>
+                Table one:
+                {renderTableOrders('Table1')}
+            </div>
+            <div>
+                Table Two:
+                {renderTableOrders('Table2')}
+            </div>
+            <div>
+                Table three:
+                {renderTableOrders('Table3')}
+            </div>
+        </>
+    );
+};
 
-export default ListComponent
+export default ListComponent;
